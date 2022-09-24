6eaa2667fd9e2ac33f1b51e38410e4b932efd2cf
Adrianna McKeown <amckeow3@uncc.edu>
2022-09-24 12:55:29 -0400

    server running on port 4000

---
+++ app/controllers/auth.controller.js
@@ -0,0 +1,3 @@
+exports.test = (req, res) => {
+    res.status(200).send({ message: "Test function successfully called!" });
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require('./app/routes/auth.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
